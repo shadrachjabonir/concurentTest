e9a5736544d7ff8c53d4009d3128e4d45934b23a
shadrachjabonir <shadrachjabonir@gmail.com>
2016-06-26 22:26:46 +0700

    init
    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by shadr on 26/06/2016.
+ */
+public class Main {
+    public static void main(String args[]){
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
