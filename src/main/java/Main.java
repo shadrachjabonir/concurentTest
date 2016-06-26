f3a8c08fdafe4845d51c66a400ccd9cbb8aedab8
shadrachjabonir <shadrachjabonir@gmail.com>
2016-06-26 23:18:35 +0700

    complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,35 @@
+import java.math.BigDecimal;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static java.lang.Thread.sleep;
+import static java.lang.management.ManagementFactory.getThreadMXBean;
+
 /**
  * Created by shadr on 26/06/2016.
  */
 public class Main {
-    public static void main(String args[]){
 
-        System.out.println(Runtime.getRuntime().availableProcessors());
+    private volatile boolean expired;
+    private final int EPSYLON = 20;
+
+    public static void main(String args[]) throws InterruptedException {
+        int nCpu = Runtime.getRuntime().availableProcessors();
+        System.out.println("cpu : " + nCpu);
+        double uCpu = 1;
+        System.out.println("utilize cpu ratio : " + uCpu);
+        long  cpuTime = System.nanoTime();
+        System.out.println("current time nano : " +cpuTime);
+        Thread.sleep(400);
+        cpuTime = System.nanoTime() - cpuTime;
+        System.out.println("delta time nano : " +cpuTime);
+
+        long waitTime = (400 * 1000000) - cpuTime;
+        System.out.println("wait time nano : " +waitTime);
+
+        Double nThreads = nCpu * uCpu * (1+ waitTime/cpuTime);
+        System.out.println("cpu * utilize cpu ratio * (1 + waitTime/ delta time nano) : ");
+        System.out.println(nThreads);
     }
+
 }
